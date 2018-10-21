069e164fbb628f3e1279d899261bd103e9ac300e
Michael Gallegos <snowysani@gmail.com>
2018-10-21 10:04:51 -0500

    Update two_one_mux_behavioral.v

--- lab7/two_one_mux_behavioral.v
+++ lab7/two_one_mux_behavioral.v
@@ -2,17 +2,15 @@
 `default_nettype none
 
 module two_one_mux(Y, A, B, S); // define the module
-
-    output reg Y; // declare the output as reg because of the 
-                // always block
+    output reg Y; // declare the output as reg because 
+                  // of the always block
     input wire A, B, S; // input wires
     
     always @(A or B or S)
         begin // block
             if (S == 1'b0) // comparison, use 2 equals
-                Y = A;
-            else
-                Y = B;
+                Y = A; // drive Y with A
+            else // otherwise S == b1
+                Y = B; // drive Y with B
         end // end the block
-        
 endmodule // end it
