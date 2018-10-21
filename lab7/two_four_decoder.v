02c40290891b63ca1ed5b2da05e3311aed5545b9
Michael Gallegos <snowysani@gmail.com>
2018-10-21 10:10:12 -0500

    Update two_four_decoder.v

--- lab7/two_four_decoder.v
+++ lab7/two_four_decoder.v
@@ -2,23 +2,21 @@
 `default_nettype none
 
 module two_four_decoder(
-    input wire [1:0] W,
-    input wire En, 
-    // y outputs 4 bit type reg 
-    output reg [3:0] Y
+    input wire [1:0] W, //2 bit W input
+    input wire En, //input wire Enable
+    output reg [3:0] Y // reg 4 bit output Y
     );
     
     always@(W or En) // Trigger when En or W changes
         begin
-            if(En == 1'b1)
-                case(W)
+            if(En == 1'b1) // if enable is 1 
+                case(W) // start the case
                     2'b00: Y = 4'b0001; // 4 bit, y[0]
                     2'b01: Y = 4'b0010; // y[1]
                     2'b10: Y = 4'b0100; // y[2]
                     2'b11: Y = 4'b1000; // light up y[3]
-                endcase
+                endcase // end the case
             else // if enable is not on, disable output
-                Y = 4'b0000;
+                Y = 4'b0000; // set them all to 0
         end
-    
 endmodule // end it
